decf96ed0c63c7db5793ea3febb27b7b25227b9b
Santhosh <santhosh.murugesan@gmail.com>
2012-06-12 23:53:21 +0100

    MySQL Support added

--- src/main/java/com/san/cloud/service/LoginService.java
+++ src/main/java/com/san/cloud/service/LoginService.java
@@ -3,6 +3,7 @@ package com.san.cloud.service;
 import com.san.cloud.pages.login.LoginInfo;
 
 import java.io.Serializable;
+import java.sql.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,10 +13,41 @@ import java.io.Serializable;
  * To change this template use File | Settings | File Templates.
  */
 public class LoginService {
-    public boolean isValid(LoginInfo loginInfo) {
-        if("admin".equals(loginInfo.getUsername()) && "admin".equals(loginInfo.getPassword())) {
-            return true;
+    public boolean isValid(LoginInfo loginInfo)  {
+        Connection con = null;
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://instance17451.db.xeround.com:11735/sanclouddb?" +
+                            "user=santhosh&password=it5compl1cat3d");
+
+            PreparedStatement stmt = con.prepareStatement("select count(id) from sanappuser where username=? and password=?");
+            stmt.setString(1, loginInfo.getUsername());
+            stmt.setString(2, loginInfo.getPassword());
+
+            ResultSet rs = stmt.executeQuery();
+            int count = 0;
+
+            if(rs.next()) {
+                count = rs.getInt(1);
+            }
+            System.out.println("Count......."+count);
+            return count > 0?true:false;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
         }
+
         return false;
     }
 }
